94f5e50c90427bbca8be3b84e330a4fbdfa1b71c
naufal rahmadan <naufalrahmadan12@gmail.com>
2025-10-08 07:29:44 +0000

    tidak rata di category dan due date

--- src/components/submit/task-form.tsx
+++ src/components/submit/task-form.tsx
@@ -166,16 +166,16 @@ export function TaskForm() {
                       <Button
                         variant={"outline"}
                         className={cn(
-                          "w-full pl-3 text-left font-normal",
+                          "w-full justify-start text-left font-normal",
                           !field.value && "text-muted-foreground"
                         )}
                       >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
                         {field.value ? (
                           format(field.value, "PPP")
                         ) : (
                           <span>Pick a date</span>
                         )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                       </Button>
                     </FormControl>
                   </PopoverTrigger>
@@ -184,7 +184,7 @@ export function TaskForm() {
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
-                      disabled={(date) => date < new Date() || date < new Date("1900-01-01")}
+                      disabled={(date) => date < new Date("1900-01-01")}
                       initialFocus
                     />
                   </PopoverContent>
