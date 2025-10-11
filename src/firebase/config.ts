040cbff16d0817a65b4353ef1a5096c7dabf08bf
naufal rahmadan <naufalrahmadan12@gmail.com>
2025-10-11 17:42:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "kreatask-backup-56531938-324cb",
+  "appId": "1:615077435379:web:e4283cf3d19117a695a486",
+  "apiKey": "AIzaSyCcjQwBIkcJjkusHY9knsASOYaOuzrbeFo",
+  "authDomain": "kreatask-backup-56531938-324cb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "615077435379"
+};
