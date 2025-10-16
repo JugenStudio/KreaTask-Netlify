11bdeec73e0e6d954589121c0629593a213a817a
naufal rahmadan <naufalrahmadan12@gmail.com>
2025-10-16 16:03:51 +0000

    hapus ini yg membuat eror dan tidak bisa di deploy The Netlify deploy er

--- src/env.ts
+++ src/env.ts
@@ -6,10 +6,10 @@ config({ path: '.env' });
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
-  SECRET_COOKIE_PASSWORD: z.string().min(32, 'Secret cookie password must be at least 32 characters long'),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().optional(),
 });
 
 export const env = envSchema.parse(process.env);
