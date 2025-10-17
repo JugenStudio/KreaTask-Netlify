6bd6054aa020d69c191b940ee6fb1ea0277b8510
naufal rahmadan <naufalrahmadan12@gmail.com>
2025-10-17 06:51:51 +0000

    kreatask-netlify-11534430:~/studio{main}$ npm run build
    > nextn@0.1.0 b

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    allowedDevOrigins: [
-      'https://3000-firebase-kreatask-netlify-1760610089432.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev'
-    ]
-  },
   images: {
     remotePatterns: [
       {
